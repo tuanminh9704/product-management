548583912bcadda5832d2706ac58405119954d10
tuanminh9704 <132085614+tuanminh9704@users.noreply.github.com>
2024-11-08 10:36:12 +0700

    Update Logging and Error Handling

--- src/middlewares/logging/logging.middleware.ts
+++ src/middlewares/logging/logging.middleware.ts
@@ -4,7 +4,7 @@ import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 export class LoggingMiddleware implements NestMiddleware {
   private logger = new Logger("HTTP");
   use(req: any, res: any, next: () => void) {
-    this.logger.log(`Logging HTTP request method: ${req.method}, url: ${req.originalUrl}, statusCode: ${res.statusCode}`);
+    this.logger.log(`Logging HTTP request method: ${req.method}, url: ${req.originalUrl}, statusCode: ${res.statusCode}, body: ${req.body}`);
     next();
   }
 }
